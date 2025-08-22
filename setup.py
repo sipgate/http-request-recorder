0a9f990931d56276a4630bea04a45c9e48c3a11a
Paul Schwörer <schwoerer@sipgate.de>
2025-08-22 14:01:24 +0200

    Revert "chore: use pyproject-based build setup"
    This reverts commit b0bd38dde254e1018050cd47ba7678a4116fc539.
    
    .pyproject-based Build setup seems to change the way the package needs
    to be imported.

---
+++ setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='http_request_recorder',
+    version='2.0.0',
+    description='A package to record an respond to http requests, primarily for use in black box testing.',
+    long_description=readme,
+    author='',
+    author_email='',
+    url='https://github.com/sipgate/http-request-recorder',
+    license=license,
+    packages=['http_request_recorder'],
+    package_data={"http_request_recorder": ["py.typed"]},
+    install_requires=[
+        'aiohttp~=3.10.10',
+    ],
+    zip_safe=False
+)
