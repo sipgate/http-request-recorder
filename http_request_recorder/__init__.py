2c0c29fe30de0e28ad520023dae2a592012e1415
Paul Schwörer <schwoerer@sipgate.de>
2025-08-26 14:05:26 +0200

    feat: add top-level export for RecordedRequest
    e.g. for use in custom request matchers

--- http_request_recorder/__init__.py
+++ http_request_recorder/__init__.py
@@ -1,3 +1,3 @@
-from .http_request_recorder import HttpRequestRecorder  # noqa: F401
+from .http_request_recorder import HttpRequestRecorder, RecordedRequest  # noqa: F401
 
-__all__ = ['HttpRequestRecorder']
+__all__ = ['HttpRequestRecorder', 'RecordedRequest']
